21dc8a3c8c35c1a19da294632b25c43e4931e714
Cihat KOÇAK <pcihatkocakks@gmail.com>
2021-11-15 14:47:00 +0300

    sidebar is complete

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,30 +1,72 @@
-import React from 'react'
-import { HomeIcon, ExploreIcon, NotificationsIcon, MessagesIcon, BookmarksIcon, ListIcon, ProfileIcon, MoreIcon, PopulerIcon, ImageIcon, GIFIcon, PollIcon, EmojiIcon, ScheduleIcon, ReplyIcon, ReTweetIcon, LikeIcon, ShareIcon, SearchIcon } from '../Icon'
+
+import SideLink from '../components/SideLink';
+import {
+    HomeIcon, ExploreIcon, NotificationsIcon, MessagesIcon, BookmarksIcon, ListIcon, ProfileIcon, MoreIcon, PopulerIcon,
+    ImageIcon, GIFIcon, PollIcon, EmojiIcon, ScheduleIcon, ReplyIcon, ReTweetIcon, LikeIcon, ShareIcon, SearchIcon
+} from '../Icon'
+import twitterLogo from "../images/twitter.svg"
+import React, { useEffect, useState } from "react";
+import UserBox from '../components/UserBox';
+
+
+const sideLinks = [
+    {
+        name: 'Home',
+        icon: HomeIcon,
+    },
+    {
+        name: 'Explore',
+        icon: ExploreIcon,
+    },
+    {
+        name: 'Notifications',
+        icon: NotificationsIcon,
+    },
+    {
+        name: 'Message',
+        icon: MessagesIcon,
+    },
+    {
+        name: 'Bookmarks',
+        icon: BookmarksIcon,
+    },
+    {
+        name: 'List',
+        icon: ListIcon,
+    },
+    {
+        name: 'Profile',
+        icon: ProfileIcon,
+    },
+    {
+        name: 'More',
+        icon: MoreIcon,
+    },
+    
+];
 
 const Sidebar = () => {
+    const [active, setActive] = useState("Home");
+    const handleMenuItemClik = (name) => {
+        setActive(name);
+    }
     return (
-        <div className="w-72 bg-blue-200">
-            <h1>Sidebar</h1>
-            <HomeIcon />
-            <ExploreIcon />
-            <NotificationsIcon />
-            <MessagesIcon />
-            <BookmarksIcon />
-            <ListIcon />
-            <ProfileIcon />
-            <MoreIcon />
+        <div className="flex flex-col justify-between w-72 bg-blue-10 px-2 ">
+            <div>
+                <div className="mt-1 mb-4 ml-1 flex items-center justify-center w-12 h-12 rounded-full hover:bg-primary-light transform transition-colors duration-200">
+                    <img src={twitterLogo} alt="Twitter Logo" className="w-9 h-9" />
+                </div>
 
-            <PopulerIcon className ="w-7 h-7" />
-            <ImageIcon className ="w-7 h-7" />
-            <GIFIcon className ="w-7 h-7" />
-            <PollIcon className ="w-7 h-7" />
-            <EmojiIcon className ="w-7 h-7" />
-            <ScheduleIcon className ="w-7 h-7" />
-            <ReplyIcon className ="w-7 h-7" />
-            <ReTweetIcon className ="w-7 h-7" />
-            <LikeIcon className ="w-7 h-7" />
-            <ShareIcon className ="w-7 h-7" />
-            <SearchIcon className ="w-7 h-7" />
+                <nav>
+                    <ul>
+                       {sideLinks.map(({name,icon}) => (
+                           <SideLink key = {name} name = {name} Icon = {icon} active={active} onMenuItemClick = {handleMenuItemClik} />
+                       ))}
+                    </ul>
+                </nav>
+                <button className="bg-primary-base hover:bg-primary-dark text-white shadow-lg rounded-full py-3 px-8 w-11/12 transform transition-colors duration-200">Tweet</button>
+            </div>
+            <UserBox />
         </div>
     )
 }
