3ea96965cc8d1edff9a4d8e07d5eb22febfd7efc
Cihat KOÇAK <pcihatkocakks@gmail.com>
2021-11-19 17:16:33 +0300

    Final

--- src/components/TweetBox.jsx
+++ src/components/TweetBox.jsx
@@ -1,14 +1,45 @@
 import React, {useState} from 'react'
 import { EmojiIcon, GIFIcon, ImageIcon, PollIcon, ScheduleIcon } from '../icons/Icon'
+import db from "../firebase";
+import { collection, doc, setDoc, serverTimestamp  } from "firebase/firestore"; 
+
+
+
+
 
 const TweetBox = () => {
     const [content, setContent] = useState('');
+
+    const sendTweet = async() => {
+        if(content !== ''){
+            
+            
+
+            const userRef = collection(db,'feed');
+            
+            await setDoc(doc(userRef),{
+                displayName: "iksipi",
+                userName:"@davsanavi",
+                content, 
+                timestamp: serverTimestamp(),
+                avatar: "https://pbs.twimg.com/profile_images/1457293343990456324/c2yR0CpQ_400x400.jpg",
+                image: "https://media.giphy.com/media/3o85xsGXVuYh8lM3EQ/giphy.gif"
+            }).then((result) => {
+                console.log("user added!")
+            }).catch((err) => {
+                console.log(err)
+            });;
+            setContent("");
+        }
+    }
+
     return (
         <div className="flex flex-col flex-1 mt-2 px-2">
             <textarea 
             className="w-full text-xl placeholder-gray-dark outline-none overflow-hidden resize-none bg-transparent" 
             placeholder="What's happening?"
-            onChange={e => setContent(e.target.value)} />
+            onChange={e => setContent(e.target.value)}
+            value = {content} />
             <div className="flex items-center justify-between">
                 <div className="flex -ml-3">
                     <div className= "flex items-center justify-center w-11 h-11 rounded-full hover:bg-gray-lightest">
@@ -28,7 +59,9 @@ const TweetBox = () => {
                     </div>
 
                 </div>
-                <button className="bg-primary-base text-white rounded-full px-4 py-2 font-medium">Tweet</button>
+                <button className="bg-primary-base text-white rounded-full px-4 py-2 font-medium"
+                onClick={sendTweet}
+                >Tweet</button>
             </div>
         </div>
     )
--- src/layout/Content.jsx
+++ src/layout/Content.jsx
@@ -1,11 +1,27 @@
-import React, {useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Divider from '../components/Divider'
 import TweetBox from '../components/TweetBox'
 import db from '../firebase'
+import { collection, onSnapshot } from "firebase/firestore";
 import { PopulerIcon } from '../icons/Icon'
+import FeedList from '../components/FeedList';
 
 const Content = () => {
-    
+    const [tweets, setTweets] = useState([]);
+
+
+
+
+    useEffect(() => {
+        onSnapshot(collection(db, 'feed'), (snapshot) => {
+            setTweets(snapshot.docs.map(doc => doc.data()));
+        });
+    }, []);
+
+//
+
+    console.log(tweets);
+
     return (
         <main className="flex-1 flex flex-col border-r border-l border-gray-extraLight">
             <header className=" sticky top-0 z-10 flex justify-between items-center p-4 border-b border-gray-extraLight bg-white">
@@ -13,13 +29,15 @@ const Content = () => {
                 <PopulerIcon className="w-6 h-6 text-primary-base" />
             </header>
             <div className="flex space-x-4 px-4 py-3">
-            <img 
-            src="https://pbs.twimg.com/profile_images/1457293343990456324/c2yR0CpQ_400x400.jpg"
-             alt="Profile"
-             className="w-11 h-11 rounded-full" />
-                 <TweetBox />
+                <img
+                    src="https://pbs.twimg.com/profile_images/1457293343990456324/c2yR0CpQ_400x400.jpg"
+                    alt="Profile"
+                    className="w-11 h-11 rounded-full" />
+                <TweetBox />
             </div>
             <Divider />
+            <FeedList tweets={tweets.sort((a,b) => b.timestamp - a.timestamp)} />
+
         </main>
     )
 }
